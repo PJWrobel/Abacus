a20de860c37343cafd879fad313a4cc73c0eaaf3
Paul Wrobel <wrobel.paulj@gmail.com>
2025-03-12 15:05:56 -0600

    not sure what i updated but here you go

---
+++ compiler/abatype.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef enum a_type {
+    void_type = 0;
+	bool,
+    u8,
+    i8,
+    u16,
+    i16,
+    u32,
+    i32,
+    u64,
+    i64,
+    //f8,  //useable?
+    //f16, //
+    f32,
+    f64,
+    tuple,
+    ptr,
+    fn,
+
+};
+
+
+int main() {}
---
+++ compiler/parse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+#include "pstr.h"
+#include "abatype.h"
+
+
+
+(void*) parse_num(pstr s, enum type) {
+    
+}
